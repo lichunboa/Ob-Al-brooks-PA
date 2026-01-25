911bcf33be9d26c15736235a62cf264b945c0721
MITCHELLCB <lcb260329@gmail.com>
2026-01-25 22:12:35 +0800

    fix(srs): resolve build error on main by excluding missing locales

--- .obsidian/plugins/obsidian-spaced-repetition/src/lang/helpers.ts
+++ .obsidian/plugins/obsidian-spaced-repetition/src/lang/helpers.ts
@@ -2,72 +2,72 @@
 
 import { moment } from "obsidian";
 
-import af from "src/lang/locale/af";
+// import af from "src/lang/locale/af";
 import ar from "src/lang/locale/ar";
 import bn from "src/lang/locale/bn";
 import cz from "src/lang/locale/cz";
-import da from "src/lang/locale/da";
-import de from "src/lang/locale/de";
+// import da from "src/lang/locale/da";
+// import de from "src/lang/locale/de";
 import en from "src/lang/locale/en";
-import enGB from "src/lang/locale/en-gb";
+// import enGB from "src/lang/locale/en-gb";
 import es from "src/lang/locale/es";
-import fr from "src/lang/locale/fr";
+// import fr from "src/lang/locale/fr";
 import hi from "src/lang/locale/hi";
 import id from "src/lang/locale/id";
-import it from "src/lang/locale/it";
+// import it from "src/lang/locale/it";
 import ja from "src/lang/locale/ja";
-import ko from "src/lang/locale/ko";
+// import ko from "src/lang/locale/ko";
 import mr from "src/lang/locale/mr";
-import nl from "src/lang/locale/nl";
+// import nl from "src/lang/locale/nl";
 import no from "src/lang/locale/no";
 import pl from "src/lang/locale/pl";
 import pt from "src/lang/locale/pt";
-import ptBR from "src/lang/locale/pt-br";
-import ro from "src/lang/locale/ro";
-import ru from "src/lang/locale/ru";
+// import ptBR from "src/lang/locale/pt-br";
+// import ro from "src/lang/locale/ro";
+// import ru from "src/lang/locale/ru";
 import sw from "src/lang/locale/sw";
 import ta from "src/lang/locale/ta";
 import te from "src/lang/locale/te";
 import th from "src/lang/locale/th";
 import tr from "src/lang/locale/tr";
 import uk from "src/lang/locale/uk";
-import ur from "src/lang/locale/ur";
-import vi from "src/lang/locale/vi";
+// import ur from "src/lang/locale/ur";
+// import vi from "src/lang/locale/vi";
 import zhCN from "src/lang/locale/zh-cn";
 import zhTW from "src/lang/locale/zh-tw";
 
 export const localeMap: { [k: string]: Partial<typeof en> } = {
-    af,
+    // af,
     ar,
     bn,
     cs: cz,
-    da,
-    de,
+    // da,
+    // de,
     en,
-    "en-gb": enGB,
+    // "en-gb": enGB,
     es,
-    fr,
+    // fr,
     hi,
     id,
-    it,
+    // it,
     ja,
-    ko,
+    // ko,
     mr,
-    nl,
+    // nl,
     nn: no,
     pl,
     pt,
-    "pt-br": ptBR,
-    ro,
-    ru,
+    // "pt-br": ptBR,
+    // ro,
+    // ru,
     sw,
     ta,
     te,
     th,
     tr,
     uk,
-    ur,
-    vi,
+    // ur,
+    // vi,
     "zh-cn": zhCN,
     "zh-tw": zhTW,
 };
